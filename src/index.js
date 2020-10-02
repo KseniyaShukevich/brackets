cd4803942dee83b33b84eb238dfae47f113e7982
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-10-02 22:02:00 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsMap = new Map(bracketsConfig);
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (bracketsMap.has(str[i]) && bracketsMap.get(str[i]) !== str[i]) {
+            arr.push(str[i]);
+        } else if (bracketsMap.has(str[i]) && bracketsMap.get(str[i]) === str[i]) {
+            if (arr[arr.length - 1] === str[i]) {
+                arr.pop();
+            } else {
+                arr.push(str[i]);
+            }
+        } else {
+            if (str[i] !== bracketsMap.get(arr.pop())) {
+                return false;
+            }
+        }
+    }
+
+    if (arr.length === 0) {
+        return true;
+    }
+
+    return false;
+};
